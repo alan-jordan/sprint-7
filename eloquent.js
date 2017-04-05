6b74843468b6641ddccd18a2d1336c8bd379be53
Alan Jordan <alanpjordan@gmail.com>
2017-04-05 12:16:48 +1200

    Added second solution

--- eloquent.js
+++ eloquent.js
@@ -1,6 +1,4 @@
-
-
-// Your code here.
+// Minimum function
 var min = function(x,y) {
   return Math.min(x,y);
 }
@@ -8,3 +6,18 @@ console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
+
+
+// Recursion function
+var isEven = function(num) {
+  /* This works out whether number is odd or even by flipping to
+  a positive number using Math.abs and recursively calling the function*/
+  if (num < 0) {return isEven(Math.abs(num));}
+  if (num === 0) {
+    return true;
+  } else if (num == 1) {
+    return false;
+  } else {
+    return isEven(num - 2);
+  }
+}
